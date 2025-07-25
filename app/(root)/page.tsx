159be8d297cb4f6046ef4d51262cf64c45bc23a9
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-25 13:49:19 +0100

    intial version

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl">Home</h1>
+    </div>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+        <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" width={144} height={30} alt="logo" />
+                </Link>
+            </nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
