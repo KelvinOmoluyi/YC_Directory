98d9bbe8673246e407b6d044931eb7a81eb61b75
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-25 20:34:42 +0100

    fixed Oauth bug

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
-import NextAuth from "next-auth"
-import Github from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Github({
-    clientId: process.env.GITHUB_CLIENT_ID!,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  })],
-})
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+});
