a55ed3c9a4f0e23121bc3417efccee54fb638a7c
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-26 17:51:36 +0100

    style and assets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-5xl">Home</h1>
-    </div>
+    <>
+      <h1 className="heading">Home</h1>
+    </>
   );
 }
