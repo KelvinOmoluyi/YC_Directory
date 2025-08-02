e48e441755aedc83b19ea010e34c1861e33d85ec
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-02 21:28:23 +0100

    queries search params modification

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,6 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERIES = defineQuery(`*[_type == "startup" && defined(slug.current)]{
+export const STARTUPS_QUERIES = defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search]{
   _id,
   title,
   slug,
@@ -11,5 +11,20 @@ export const STARTUPS_QUERIES = defineQuery(`*[_type == "startup" && defined(slu
   views,
   description,
   category,
+  image
+}`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+    _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  },
+  views,
+  description,
+  category,
+  pitch,
   image
 }`);
