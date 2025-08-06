029dc17eca75f120196a2524235489214b7596db
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-06 22:25:53 +0100

    adjusted startup query to fetch description

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -20,11 +20,25 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $
   slug,
   _createdAt,
   author -> {
-    _id, name, image, bio
+    _id, name, image, bio, username
   },
   views,
   description,
   category,
   pitch,
   image
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+  id, views
+  }`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && _id == $id][0]{
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
 }`);
