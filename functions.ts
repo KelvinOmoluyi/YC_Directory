90a398704ee4d65d3570e414d11473761abc6003
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-06 22:13:30 +0100

    error message parsing to JSON function created

--- functions.ts
+++ functions.ts
@@ -5,3 +5,7 @@ export function formatDate(date: string) {
         day: "numeric",
     });
 }
+
+export function parseServerActionResponse<T>(response: T) {
+    return JSON.parse(JSON.stringify(response));
+}
